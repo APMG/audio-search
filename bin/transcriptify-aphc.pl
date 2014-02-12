fb2f64c17361f9b717859a950518f91ab5e77d69
Peter Karman <pkarman@mpr.org>
2014-02-12 14:12:04 -0600

    make the transcriptify work in batch; more fixes for PHC syntax

--- bin/transcriptify-aphc.pl
+++ bin/transcriptify-aphc.pl
@@ -9,35 +9,45 @@ use strict;
 use warnings;
 use File::Slurp;
 
-my $file = shift or die "$0 file";
-my @buf = read_file($file);
-my @new;
-
-for my $line (@buf) {
-    $line =~ s/^\xEF\xBB\xBF//;    # .docx format inserts BOM sequence
-
-    $line =~ s/\[/\(/gs;
-    $line =~ s/\]/\)/gs;
-    $line =~ s!\(k\)! k!gm;
-    $line =~ s!\(([A-Z\ \,]+)\)!\n#background $1\n!g; # all-CAPS effects in brackets
-    $line =~ s!^([A-Z]+)\:!#speaker $1\n!g;        # comment speaker initials
-    $line =~ s!^([\d\.]+)\s*!#timestamp $1\n!g;    # timing marks
-
-    # special case for APHC
-    $line =~ s!^GK[\,A-Z]+:!#speaker GK\n!gm;
-
-    push @new, $line;
-    push @new, "\n";
+if ( !@ARGV ) {
+    die "$0 file [...fileN]\n";
 }
 
-# create new buf and zap multi-line patterns
-my $new_buf = join( '', @new );
+for my $file (@ARGV) {
+    my @buf = read_file($file);
+    my @new;
 
-# timestamps followed by segment title
-$new_buf =~ s,(#timestamp \S+\n)\n.+?\n,$1,sg;
+    for my $line (@buf) {
+        $line =~ s/^\xEF\xBB\xBF//;    # .docx format inserts BOM sequence
 
-# normalize whitespace a little
-$new_buf =~ s,\n\n+,\n\n,g;
+        $line =~ s/\[/\(/gs;
+        $line =~ s/\]/\)/gs;
+        $line =~ s!\(k\)! k!gm;
 
-write_file( $file, $new_buf );
+        $line =~ s!^(APPLAUSE|LAUGHTER)!#background $1!g;
+
+        # all-CAPS effects in brackets
+        $line =~ s!\(([A-Z\ \,]+)\)!\n#background $1\n!g;
+
+        $line =~ s!^([A-Z\ ]+)\:!#speaker $1\n!g;        # speaker names
+        $line =~ s!^([\d\.\:]+)\s*!#timestamp $1\n!g;    # timing marks
+
+        # special case for APHC
+        $line =~ s!^GK[\,A-Z]+:!#speaker GK\n!gm;
+
+        push @new, $line;
+        push @new, "\n";
+    }
+
+    # create new buf and zap multi-line patterns
+    my $new_buf = join( '', @new );
+
+    # timestamps followed by segment title
+    $new_buf =~ s,(#timestamp \S+\n)\n+[^#].+?\n,$1,sg;
+
+    # normalize whitespace a little
+    $new_buf =~ s,\n\n+,\n\n,g;
+
+    write_file( $file, $new_buf );
+}
 
