2ac8f3aee10d6ad4479e727300e690450539fbf6
Peter Karman <pkarman@mpr.org>
2014-05-14 10:35:05 -0500

    add endpoints for transcript features

--- app/TQApp/lib/TQApp/Controller/V1/Media.pm
+++ app/TQApp/lib/TQApp/Controller/V1/Media.pm
@@ -9,4 +9,32 @@ __PACKAGE__->config(
     primary_key => 'uuid',
 );
 
+# override GET /<uuid>/bar for some specific 'bar' values
+around 'two_args_GET' => sub {
+    my ( $super_method, $self, $c, $id, $rel ) = @_;
+    return if $c->stash->{fetch_failed};
+
+    if ( $self->can($rel) ) {
+        $self->$rel($c);
+    }
+    else {
+        $self->$super_method( $c, $id, $rel );
+    }
+};
+
+sub text {
+    my ( $self, $c ) = @_;
+    my $media = $c->stash->{object};
+    my $text  = $media->transcript_as_text;
+    $self->status_ok( $c, entity => { text => $text, uuid => $media->uuid } );
+}
+
+sub keywords {
+    my ( $self, $c ) = @_;
+    my $media = $c->stash->{object};
+    my $kw    = $media->keywords;
+    $self->status_ok( $c,
+        entity => { keywords => $kw, uuid => $media->uuid } );
+}
+
 1;
