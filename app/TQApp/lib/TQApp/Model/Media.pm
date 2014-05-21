db4633a36fec6b9bf6b4fb8f8fb4aa643c6b07f8
Peter Karman <pkarman@mpr.org>
2014-05-21 16:45:47 -0500

    apply cxc-minimal only to search() not count()

--- app/TQApp/lib/TQApp/Model/Media.pm
+++ app/TQApp/lib/TQApp/Model/Media.pm
@@ -20,13 +20,23 @@ sub make_query {
     my $user = $c->stash->{user} or confess "User required";
     push @{ $q->{query} }, ( user_id => $user->id );
 
-    # trim results if asked
-    # cxc-minimal to avoid pulling transcript when not needed (list view)
-    if ( $c->req->params->{'cxc-minimal'} ) {
-        $q->{select} = [qw( uuid name updated_at status uri )];
+    return $q;
+}
+
+sub search {
+    my $self = shift;
+    my (@arg) = @_;
+    if ( $arg[0] and ref $arg[0] eq 'HASH' ) {
+        my $c = $self->context;
+
+        # trim results if asked
+        # cxc-minimal to avoid pulling transcript when not needed (list view)
+        if ( $c->req->params->{'cxc-minimal'} ) {
+            $arg[0]->{select} = [qw( uuid name updated_at status uri )];
+        }
     }
 
-    return $q;
+    return $self->next::method(@arg);
 }
 
 1;
