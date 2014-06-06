89b1bfba3fe57be801070b7343e4967d94664120
Peter Karman <pkarman@mpr.org>
2014-06-06 16:25:14 -0500

    refactor, add julius-adaptation pkg

--- bin/install-deps.pl
+++ bin/install-deps.pl
@@ -6,9 +6,10 @@ use TQ::Utils qw( run_it );
 
 # download sf.net dependencies and install them
 
-my $verb               = 1;
-my $acoustic_model_pkg = 'acoustic-model-0.9.tar.gz';
-my $language_model_pkg = 'language-model-0.9.tar.gz';
+my $verb                  = 1;
+my $acoustic_model_pkg    = 'acoustic-model-0.9.tar.gz';
+my $language_model_pkg    = 'language-model-0.9.tar.gz';
+my $julius_adaptation_pkg = 'julius-adaptation-0.9.tar.gz';
 
 sub get_sf_url {
     my $file = shift or die "file required";
@@ -18,23 +19,20 @@ sub get_sf_url {
         $file );
 }
 
-# models unpack into etc/
-run_it(
-    sprintf(
-        "wget -q %s && tar xvfz %s && rm %s",
-        get_sf_url($acoustic_model_pkg), $acoustic_model_pkg,
-        $acoustic_model_pkg,
-    ),
-    $verb
-);
-run_it(
-    sprintf(
-        "wget -q %s && tar xvfz %s && rm %s",
-        get_sf_url($language_model_pkg), $language_model_pkg,
-        $language_model_pkg,
-    ),
-    $verb
-);
+sub install_from_sf {
+    my $pkg = shift or die "package required";
+    run_it(
+        sprintf(
+            "wget -q %s && tar xvfz %s && rm %s",
+            get_sf_url($pkg), $pkg, $pkg,
+        ),
+        $verb
+    );
+}
+
+install_from_sf($acoustic_model_pkg);
+install_from_sf($language_model_pkg);
+install_from_sf($julius_adaptation_pkg);
 
 # run the cantab install script
 run_it( "sh install.sh", $verb );
