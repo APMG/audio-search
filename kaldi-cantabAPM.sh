1b452824cfa81d77c54d28ac8975898550ead2b6
Peter Karman <pkarman@mpr.org>
2014-06-26 10:20:15 -0500

    tweek output labels

--- kaldi-cantabAPM.sh
+++ kaldi-cantabAPM.sh
@@ -40,7 +40,7 @@ fi
 
 mkdir -p $WORK
 
-echo "Running audio segmentation"
+echo "==================== Running audio segmentation ====================="
 cp $INPUT $WORK/$wavName.wav
 echo $wavName | perl -pe "s:^:$WORK/tmp-rec/:" | perl -pe 's:^(.*)/.*$:$1:' | sort -u | xargs --no-run-if-empty mkdir -p
 
@@ -66,7 +66,7 @@ cat $WORK/tmp-rec/$wavName.pem | egrep -v '^;;' | while read dummy chan spkr ini
   fi
 done
 
-echo "Preparing data"
+echo "=================== Preparing data =================="
 export LC_ALL=C
 dataPrep=$WORK/dataPrep
 mkdir -p $dataPrep
@@ -79,14 +79,14 @@ paste $dataPrep/nums $dataPrep/nums > $dataPrep/utt2spk
 cd $PREFIX
 cat $dataPrep/utt2spk | sort -k 2 | utils/utt2spk_to_spk2utt.pl > $dataPrep/spk2utt
 
-echo "Decoding"
+echo "================== Decoding =================="
 decodeDirF=exp/tri3/decode-$$
 decodeDir=exp/tri3_mmi_b0.1/decode-$$
 steps/decode_fmllr.sh --nj $decode_nj --cmd "$decode_cmd" --acwt $amw --skip_scoring true exp/tri3/graph $dataPrep $decodeDirF
 steps/decode.sh --transform-dir $decodeDirF --nj $decode_nj --cmd "$decode_cmd" --acwt $amw --skip_scoring true exp/tri3/graph $dataPrep $decodeDir
 lattice-1best --lm-scale=$lmw "ark:gunzip -c $decodeDir/lat.*.gz|" ark:- | lattice-align-words exp/lang/phones/word_boundary.int exp/tri3_mmi_b0.1/final.mdl ark:- ark:- | nbest-to-ctm ark:- - | utils/int2sym.pl -f 5 exp/lang/words.txt > $WORK/timings.all.txt
 
-echo "Writing output"
+echo "================== Writing output ================"
 echo "#!MLF!#" > $WORK/tmp.mlf
 scale=10000000
 cat $dataPrep/wav.scp | while read line; do
